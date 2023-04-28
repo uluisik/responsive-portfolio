f7572ca09c584143f56752e285db63d7b74b15cd
ahmet <ahmet55uluisik05@gmail.com>
2023-04-29 00:25:24 +0300

    created pricingCard component

---
+++ src/components/pricingCard/PricingCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./PricingCardStyles.css";
+import { Link } from "react-router-dom";
+const PricingCard = () => {
+  return (
+    <div className="pricing">
+      <div className="card-container">
+        <div className="card">
+          <h3>- Basic -</h3>
+          <span className="bar"></span>
+          <p className="btc">$ 100</p>
+          <p>- 3 Days -</p>
+          <p>- 3 Pages -</p>
+          <p>- Featured -</p>
+          <p>- Responsive Design -</p>
+          <Link to="/contact" className="btn">
+            PURCHASE NOW
+          </Link>
+        </div>
+        <div className="card">
+          <h3>- Basic -</h3>
+          <span className="bar"></span>
+          <p className="btc">$ 100</p>
+          <p>- 3 Days -</p>
+          <p>- 3 Pages -</p>
+          <p>- Featured -</p>
+          <p>- Responsive Design -</p>
+          <Link to="/contact" className="btn">
+            PURCHASE NOW
+          </Link>
+        </div>
+        <div className="card">
+          <h3>- Basic -</h3>
+          <span className="bar"></span>
+          <p className="btc">$ 100</p>
+          <p>- 3 Days -</p>
+          <p>- 3 Pages -</p>
+          <p>- Featured -</p>
+          <p>- Responsive Design -</p>
+          <Link to="/contact" className="btn">
+            PURCHASE NOW
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PricingCard;
--- src/router/Project.js
+++ src/router/Project.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import HeroImg2 from "../components/heroImg2/HeroImg2";
+import PricingCard from "../components/pricingCard/PricingCard";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS." text="Some of most recent works" />
+      <PricingCard />
       <Footer />
     </div>
   );
