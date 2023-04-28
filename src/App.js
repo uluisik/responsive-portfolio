04998a78ce68b72ee0fbd544e3214b4ed0b37d68
ahmet <ahmet55uluisik05@gmail.com>
2023-04-28 19:46:51 +0300

    created pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import React from "react";
 import "./index.css";
+import Home from "./router/Home";
+import Contact from "./router/Contact";
+import About from "./router/About";
+import Project from "./router/Project";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>aaaa</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
