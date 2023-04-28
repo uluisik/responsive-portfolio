b5d69e8df1580c0247cb091eb96db7d29604717f
ahmet <ahmet55uluisik05@gmail.com>
2023-04-29 01:39:34 +0300

    created work component  and fixed WorkCard componet

--- src/router/Project.js
+++ src/router/Project.js
@@ -3,12 +3,14 @@ import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import HeroImg2 from "../components/heroImg2/HeroImg2";
 import PricingCard from "../components/pricingCard/PricingCard";
+import Work from "../components/work/Work";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS." text="Some of most recent works" />
+      <Work />
       <PricingCard />
       <Footer />
     </div>
